97b5647abf904e4b45e41a1016106c503b514a27
Markus Prand <58184230+markusprand@users.noreply.github.com>
2020-11-22 00:05:42 +0100

    Dashboard bis auf Graph fertig, SelectCountry fertig, About fehlt Login fehlt

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,17 +1,22 @@
 var app = (function(){
     'use strict'; // execute javascript in strict mode. Eg: usage of undeclared variables is not allowed.
 
+    var url = 'https://covid-api.mmediagroup.fr/v1/cases';
+
     let initDashboard = function() {
-        requestDataWithAjax();
+        requestDataForCountry('Global');
+        requestDataForCountryList();
     }
 
-    let requestDataWithAjax = function(){
+    let initSelectCountry = function() {
+        requestDataForCountryList();
+    }
 
-        fetch('https://covid-api.mmediagroup.fr/v1/cases?country=Global', {
-            mode: 'cors',
-            headers: {
-              'Access-Control-Allow-Origin':'*'
-            }})
+    let requestDataForCountry = function(country){
+        console.log("Selected country: " + country);
+        let urlCountry = url + '?country=' + country;
+        //console.log("url: " + url);
+        fetch(urlCountry)
             .then(function(response) {
                 if(response.status !== 200){
                     console.log("Error: " + response.status);
@@ -20,42 +25,75 @@ var app = (function(){
                 return response.json();//  Get the text in the response
             })
             .then(function(responseText) {
-                console.log('Request successful');
-                //renderRequestedData(responseText.message);
+                console.log('Country Request successful');
+                renderRequestedDataCases(country, responseText);
             })
             .catch(function(error) {
-                console.log('Request failed', error)
+                console.log('Country Request failed', error)
                 // do something with error message
             });
-        // Ajax call with XMLHttpRequest Object
-        let xhttp = new XMLHttpRequest();
-
-        xhttp.onreadystatechange = responseHandler;
-        xhttp.open("GET", "https://covid-api.mmediagroup.fr/v1/cases?country=Global", true);
-        xhttp.send();   
-
     }
 
-    // function that handles ajax response
-    let responseHandler = function(){
-        if(this.readyState == 4 && this.status == 200){
-            let data = JSON.parse(this.responseText);
-            console.log(data.message);
-            // renderRequestedData(data.message);
+    let requestDataForCountryList = function(){
+        //console.log("url: " + url);
+        fetch(url)
+            .then(function(response) {
+                if(response.status !== 200){
+                    console.log("Error: " + response.status);
+                    return;
+                }
+                return response.json();//  Get the text in the response
+            })
+            .then(function(responseText) {
+                console.log('Country-List Request successful');
+                //console.log(responseText);
+                var path = window.location.pathname;
+                var page = path.split("/").pop();
+                console.log( page );
+                if (page == "index.html") {
+                    renderRequestedDataCasesList(responseText);
+                } else if (page == "selectCountry.html") {
+                    renderRequestedDataCountryList(responseText);
+                }
+
+                
+            })
+            .catch(function(error) {
+                console.log('Country-List Request failed', error)
+                // do something with error message
+            });
+    }
+
+
+    let renderRequestedDataCases = function(country,data){
+        let selectedCountry = document.getElementById("selected-country");
+        let confirmed = document.getElementById("confirmed-cases");
+        let active = document.getElementById("active-cases");
+        let recovered = document.getElementById("recovered-cases");
+        let deaths = document.getElementById("deaths");
+
+        selectedCountry.innerHTML = country;
+        confirmed.innerHTML = data.All.confirmed;
+        recovered.innerHTML = data.All.recovered;
+        deaths.innerHTML = data.All.deaths;
+        active.innerHTML = data.All.confirmed - data.All.recovered - data.All.deaths;
+    }
+
+    let renderRequestedDataCasesList = function(data) {
+        let countryList = document.getElementById("country-list");
+        for(let key in data){
+            let tableItem = _createCasesTableItem(key, data);
+            //console.log(key);
+            countryList.append(tableItem);
         }
     }
 
-    let renderRequestedData = function(data){
-        let listContainer = document.getElementById("item-list");   //get the ul element
-        let count = 0;
+    let renderRequestedDataCountryList = function(data) {
+        let countryList = document.getElementById("country-list-all");
         for(let key in data){
-            if(count === 10){   //render only 10 dogs
-                break;
-            }
-
-            let listItem = _createListItem(key, "Add to Wishlist");
-            listContainer.append(listItem);
-            count++;
+            let tableItem = _createAllCountryTableItem(key, "+");
+            //console.log(key);
+            countryList.append(tableItem);
         }
     }
 
@@ -63,39 +101,86 @@ var app = (function(){
      * Creates a list element with specific text content and btn text
      * Returns the created element
      */
-    let _createListItem = function(itemText, btnText){
-        let li = document.createElement("li");      // creates a list element (node)
-        let btn = document.createElement("button"); // creates a button element
+    let _createCasesTableItem = function(country, data){
 
-        li.textContent = itemText;      // sets the textcontents of li
-        btn.classList.add("btn-light"); // adds a class to the button element
-        btn.textContent = btnText;      // sets the button text
-        btn.addEventListener("click", function(){ app.addItemToWishlist(this); });  // adds an eventlistener to the button with a public function from app
+        let tr = document.createElement("tr");
+        let tdCountry = document.createElement("td");
+        let tdConfirmed = document.createElement("td");
+        let tdActive = document.createElement("td");
+        let tdRecovered = document.createElement("td");
+        let tdDeaths = document.createElement("td");
 
-        li.append(btn); // appends the list element with the button
+        tdCountry.textContent = country;
+        tdConfirmed.textContent = data[country].All.confirmed;
+        tdRecovered.textContent = data[country].All.recovered;
+        tdDeaths.textContent = data[country].All.deaths;
+        tdActive.textContent = data[country].All.confirmed - data[country].All.recovered - data[country].All.deaths;
+        tr.addEventListener("click", function(){app.requestDataForCountry(country);});
 
-        return li;
+        tr.append(tdCountry, tdConfirmed, tdActive, tdRecovered, tdDeaths); 
+        return tr;
     }
 
-    let addItemToWishlist = function(btnElement){
-        let parentListItem = btnElement.closest("li");
-        let clonedItem = parentListItem.cloneNode(true);    //clone the li node -> this removes all eventListeners from childnodes!
+    let _createAllCountryTableItem = function(country, btnText){
 
-        let btn = clonedItem.getElementsByTagName("button")[0]; //get the button element
+        let tr = document.createElement("tr");
+        let tdCountry = document.createElement("td");
+        let tdBtn = document.createElement("td");
+        let btn = document.createElement("btn");
+        
+        tdCountry.textContent = country;
+        btn.textContent = btnText;
+        btn.addEventListener("click", function(){app.addCountryToWatchlist(this);});
+
+        tdBtn.append(btn);
+        tr.append(tdCountry, tdBtn); 
+        return tr;
+    }
+
+    let addCountryToWatchlist = function(btnElement){
+        let parentListItem = btnElement.closest("tr");
+        let clonedItem = parentListItem.cloneNode(true);
+
+        let btn = clonedItem.getElementsByTagName("btn")[0];
         btn.textContent = "x";
-        btn.addEventListener("click", function(){ removeItemFromList(this); })   //click event that removes the list element
+        btn.addEventListener("click", function(){ removeItemFromList(this); })
 
-        document.getElementById("wishlist").append(clonedItem); //append li element to wishlist
+        document.getElementById("country-list-selected").append(clonedItem);
     }
 
     let removeItemFromList = function(btnElement){
-        btnElement.closest("li").remove();
+        btnElement.closest("tr").remove();
     }
+
+    let searchCountryList = function() {
+        // Declare variables
+        var input, filter, table, tr, td, i, txtValue;
+        input = document.getElementById("country-search");
+        filter = input.value.toUpperCase();
+        table = document.getElementById("country-list");
+        tr = table.getElementsByTagName("tr");
+      
+        // Loop through all table rows, and hide those who don't match the search query
+        for (i = 0; i < tr.length; i++) {
+          td = tr[i].getElementsByTagName("td")[0];
+          if (td) {
+            txtValue = td.textContent || td.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+              tr[i].style.display = "";
+            } else {
+              tr[i].style.display = "none";
+            }
+          }
+        }
+      }
 
     //public functions and variables
     return {
-        requestDataWithAjax: requestDataWithAjax,
-        addItemToWishlist: addItemToWishlist
+        requestDataForCountry: requestDataForCountry,
+        addCountryToWatchlist: addCountryToWatchlist,
+        initDashboard: initDashboard,
+        initSelectCountry: initSelectCountry,
+        searchCountryList: searchCountryList
     }
 }());
 

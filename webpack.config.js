2dd190902fb2ad514945bc026d31b7edd0dcb471
Markus Prand <58184230+markusprand@users.noreply.github.com>
2020-12-14 23:01:42 +0100

    WebPack-Test

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/javascript/main.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
+
+// module.exports = {
+//   mode: 'development',
+//   entry: path.join(__dirname, 'src', 'index'),
+//   watch: true,
+//   output: {
+//     path: path.join(__dirname, 'dist'),
+//     publicPath: '/dist/',
+//     filename: "bundle.js",
+//     chunkFilename: '[name].js'
+//   },
+//   module: {
+//     rules: [{
+//       test: /.jsx?$/,
+//       include: [
+//         path.resolve(__dirname, 'src')
+//       ],
+//       exclude: [
+//         path.resolve(__dirname, 'node_modules')
+//       ],
+//       loader: 'babel-loader',
+//       query: {
+//         presets: [
+//           ["@babel/env", {
+//             "targets": {
+//               "browsers": "last 2 chrome versions"
+//             }
+//           }]
+//         ]
+//       }
+//     }]
+//   },
+  // resolve: {
+  //   extensions: ['.json', '.js', '.jsx']
+  // },
+  // devtool: 'source-map',
+  // devServer: {
+  //   contentBase: path.join(__dirname, '/dist/'),
+  //   inline: true,
+  //   host: 'localhost',
+  //   port: 8080,
+  // }
+// };
